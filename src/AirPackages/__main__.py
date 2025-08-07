a40b1849b35cdba25366bafb71d37635d7a3959b
Audrey M. Roy Greenfeld <audrey@feldroy.com>
2025-08-07 11:53:50 +0800

    Add starter Air site

--- src/AirPackages/__main__.py
+++ src/AirPackages/__main__.py
@@ -1,4 +1,16 @@
 from .cli import app
+import air
+
+app = air.Air()
+
+
+@app.get("/")
+async def index():
+    return air.layouts.mvpcss(
+        air.H1("AirPackages"),
+        air.P("A directory of libraries, tools, reusable components, and example Air sites to help you with your Air projects")
+    )
+
 
 if __name__ == "__main__":
     app()
